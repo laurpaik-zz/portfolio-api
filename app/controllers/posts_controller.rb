11234b01aade17980c39a6eb0c4478f31f05e7ba
laurpaik <laurpaik@gmail.com>
2017-06-14 15:34:56 -0400

    Comment out code affected by relationship
    - For testing reasons

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :update, :destroy]
+class PostsController < OpenReadController
+  before_action :set_post, only: [:update, :destroy]
 
   # GET /posts
   def index
@@ -12,39 +12,39 @@ class PostsController < ApplicationController
 
   # GET /posts/1
   def show
-    render json: @post
+    render json: Post.find(params[:id])
   end
 
   # POST /posts
   def create
-    @post = Post.new(post_params)
-
-    if @post.save
-      render json: @post, status: :created
-    else
-      render json: @post.errors, status: :unprocessable_entity
-    end
+    # @post = current_user.posts.build(post_params)
+    #
+    # if @post.save
+    #   render json: @post, status: :created
+    # else
+    #   render json: @post.errors, status: :unprocessable_entity
+    # end
   end
 
   # PATCH/PUT /posts/1
   def update
-    if @post.update(post_params)
-      render json: @post
-    else
-      render json: @post.errors, status: :unprocessable_entity
-    end
+    # if @post.update(post_params)
+    #   head :no_content
+    # else
+    #   render json: @post.errors, status: :unprocessable_entity
+    # end
   end
 
   # DELETE /posts/1
   def destroy
-    @post.destroy
-
-    head :no_content
+    # @post.destroy
+    #
+    # head :no_content
   end
 
   # Use callbacks to share common setup or constraints between actions.
   def set_post
-    @post = Post.find(params[:id])
+    @post = current_user.examples.find(params[:id])
   end
   private :set_post
 
