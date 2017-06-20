c1451d9ffc0604aa559b33d849084e08f61ab66b
laurpaik <laurpaik@gmail.com>
2017-06-20 15:37:26 -0400

    Fix type for `body` attribute
    - change from string to text

--- db/migrate/20170612143722_create_posts.rb
+++ db/migrate/20170612143722_create_posts.rb
@@ -4,7 +4,7 @@ class CreatePosts < ActiveRecord::Migration[5.0]
   def change
     create_table :posts do |t|
       t.string :title, null: false
-      t.string :body, null: false
+      t.text :body, null: false
       t.date :date_posted, null: false
 
       t.timestamps
