2857916447d39291a88ff51d7f793ada815fd5ed
laurpaik <laurpaik@gmail.com>
2017-07-07 10:28:33 -0400

    Revert "Initial scaffold for projects"
    This reverts commit 16bb405cc402d5cf4b2a491347d8b9a01474d144.

--- app/serializers/project_serializer.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-class ProjectSerializer < ActiveModel::Serializer
-  attributes :id, :title, :link, :front_end, :back_end, :description, :image, :tech
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :projects
   resources :posts, except: [:new, :edit]
   resources :examples, except: [:new, :edit]
   post '/sign-up' => 'users#signup'
